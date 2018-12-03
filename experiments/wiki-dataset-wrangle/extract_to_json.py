2c76f0d5c55834dbcef1ef08ef716a11057fd7da
looselycoupled <allen.leis@gmail.com>
2018-12-03 13:08:53 -0500

    moves wiki extraction code

--- experiments/wiki-dataset-wrangle/extract_to_json.py
+++ experiments/wiki-dataset-wrangle/extract_to_json.py
@@ -18,12 +18,12 @@ def files():
 class WikidataIterator(object):
     def __init__(self, path, group_size, instance):
         self.path = path
-        self.i_to_ans = {}
         self.group_size = group_size
         self.instance = instance
+        self.lookups = {}
 
-    def _doc_iterator(self, path):
-        files = sorted(glob(path + "/*/*"))[:100]
+    def _doc_iterator(self):
+        files = sorted(glob(self.path + "/*/*"))
 
         for idx, path in enumerate(files):
             if idx % self.group_size == self.instance:
@@ -52,19 +52,31 @@ class WikidataIterator(object):
         parsed = urlparse.urlparse(url)
         curid = urlparse.parse_qs(parsed.query)["curid"][0]
 
-        response = requests.get(api_url.format(curid))
+        for _ in range(3):
+            response = requests.get(api_url.format(curid))
+            if response.status_code == 200:
+                break
+            print("WARN: _fetch_title encountered status code {}".format(response.status_code))
+            time.sleep(2)
+
+        if response.status_code != 200:
+            print("WARN: could not retrieve metainfo for curid={}".format(curid))
+            raise Exception("Wikimedia API Request Limiter")
+
         data = response.json()
         canonicalurl = data["query"]["pages"][curid]["canonicalurl"]
-        return canonicalurl.split("/")[-1]
+        return curid, canonicalurl.split("/")[-1]
 
 
     @property
     def docs(self):
-        for doc in self._doc_iterator(self.path):
+        for doc in self._doc_iterator():
             try:
-                ans = self._fetch_title(doc["url"])
+                curid, ans = self._fetch_title(doc["url"])
+                self.lookups[curid] = ans
             except Exception as e:
-                print(e)
+                print("WARN: sleeping for 5 seconds to cool down")
+                time.sleep(5)
                 continue
 
             for para in self._para_iterator(doc["text"]):
@@ -72,6 +84,8 @@ class WikidataIterator(object):
                     "ans": ans,
                     "text": para
                 })
+
+
 
 
 if __name__ == '__main__':
@@ -90,3 +104,6 @@ if __name__ == '__main__':
 
     elapsed = int(time.time() - start)
     print("INFO: finished in {} seconds".format(elapsed))
+
+    with open("curid_lookups.{}.json".format(instance), "w") as f:
+        f.write(json.dumps(extractor.lookups))
