9d515c1ce6281a31d7b54fcd1ca35a047a6a0227
looselycoupled <allen.leis@gmail.com>
2018-12-10 00:26:54 -0500

    try/except potential fix for codalab issues

--- src/qanta/server.py
+++ src/qanta/server.py
@@ -11,19 +11,23 @@ BUZZ_THRESHOLD = 0.3
 
 
 def guess_and_buzz(tfidf_model, dan_model, question_text):
-    tfidf_guesses = tfidf_model.guess([question_text], BUZZ_NUM_GUESSES)[0]
-    dan_guesses = dan_model.guess(question_text, BUZZ_NUM_GUESSES)
+    try:
+        tfidf_guesses = tfidf_model.guess([question_text], BUZZ_NUM_GUESSES)[0]
+        dan_guesses = dan_model.guess(question_text, BUZZ_NUM_GUESSES)
 
-    question_len = len(question_text.split(" "))
+        question_len = len(question_text.split(" "))
 
-    if question_len < 50:
-        # print("INFO: replying with TFIDF")
-        scores = [guess[1] for guess in tfidf_guesses]
-        buzz = scores[0] / sum(scores) >= BUZZ_THRESHOLD
-        return tfidf_guesses[0][0], buzz
+        if question_len < 50:
+            # print("INFO: replying with TFIDF")
+            scores = [guess[1] for guess in tfidf_guesses]
+            buzz = scores[0] / sum(scores) >= BUZZ_THRESHOLD
+            return tfidf_guesses[0][0], buzz
 
-    # print("INFO: replying with DAN")
-    return dan_guesses, True
+        # print("INFO: replying with DAN")
+        return dan_guesses, True
+
+    except Exception as e:
+        return "Error", False
 
 
 
